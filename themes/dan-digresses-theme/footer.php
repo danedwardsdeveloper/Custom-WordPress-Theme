48d29e250ac40dae6999143b2ae0d806c2acc200
Dan Edwards <daniel.edwards96@yahoo.com>
2024-05-27 21:01:17 +0100

    Divide header and footer

---
+++ themes/dan-digresses-theme/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <span>&copy; 2024 Company Name</span>
+    <ul>
+        <li>
+            <a href="" target="_blank">Terms of service</a>
+        </li>
+        <li>•</li>
+        <li><a href="" target="_blank">Privacy policy</a></li>
+    </ul>
+</footer>
+</div>
+<?php
+wp_footer();
+?>
+</body>
+
+</html>
---
+++ themes/dan-digresses-theme/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+
+<head>
+    <title>Dan Digresses</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <div id="root">
+        <header class="header">
+            <div class="brand-logo-container">
+                <span class="brand-logo">Brand logo</span>
+            </div>
+            <div class="search-container">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images-icons/icons/google-search-icon.svg" alt="" id="search-icon" />
+            </div>
+        </header>
