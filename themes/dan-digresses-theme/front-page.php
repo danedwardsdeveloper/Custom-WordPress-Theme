504a0dec5bfe70312f26b9145f533ea46eac0e25
Dan Edwards <daniel.edwards96@yahoo.com>
2024-05-28 17:56:35 +0100

    Start single.php

--- themes/dan-digresses-theme/front-page.php
+++ themes/dan-digresses-theme/front-page.php
@@ -63,37 +63,38 @@ get_header();
                     the_post();
                     $permalink = get_permalink();
             ?>
-                    <a href="<?php echo $permalink; ?>">
-                        <div class="article-preview-card">
+                    <div class="article-preview-card">
 
-                            <?php
-                            $featured_image = get_the_post_thumbnail_url($post->ID, 'small');
-                            $featured_image_alt = get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true);
-                            ?>
+                        <?php
+                        $featured_image = get_the_post_thumbnail_url($post->ID, 'small');
+                        $featured_image_alt = get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true);
+                        ?>
 
-                            <?php if ($featured_image) : ?>
-                                <img src="<?php echo esc_url($featured_image); ?>" alt="<?php echo esc_attr($featured_image_alt); ?>">
-                            <?php endif; ?>
+                        <?php if ($featured_image) : ?>
+                            <img src="<?php echo esc_url($featured_image); ?>" alt="<?php echo esc_attr($featured_image_alt); ?>">
+                        <?php endif; ?>
+                        <span class="article-preview-title">
+                            <a href="<?php echo $permalink ?>">
+                                <?php the_title(); ?>
+                            </a>
+                        </span>
 
-                            <span class="article-preview-title"><?php the_title(); ?></span>
+                        <span class="article-preview-content"><?php echo get_excerpt(); ?></span>
 
-                            <span class="article-preview-content"><?php echo get_excerpt(); ?></span>
-
-                            <div class="article-preview-card-footer">
-                                <div class="article-preview-tag">
-                                    <?php
-                                    $tags = get_the_tags();
-                                    if ($tags) {
-                                        foreach ($tags as $tag) {
-                                            echo '<span>' . $tag->name . '</a></tag>';
-                                        }
+                        <div class="article-preview-card-footer">
+                            <div class="article-preview-tag">
+                                <?php
+                                $tags = get_the_tags();
+                                if ($tags) {
+                                    foreach ($tags as $tag) {
+                                        echo '<span>' . $tag->name . '</a></tag>';
                                     }
-                                    ?>
-                                </div>
-                                <span class="article-preview-date">03 March 2024</span>
+                                }
+                                ?>
                             </div>
+                            <span class="article-preview-date">03 March 2024</span>
                         </div>
-                    </a>
+                    </div>
             <?php
                 }
             }
--- themes/dan-digresses-theme/single.php
+++ themes/dan-digresses-theme/single.php
@@ -1,65 +1,55 @@
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 
 <main>
-    <div class="column-layout">
-        <aside>
-            <ul>
-                <li>Article 1</li>
-                <li>Article 2</li>
-                <li>Article 3</li>
-                <li>Article 4</li>
-                <li>Article 5</li>
-                <li>Article 6</li>
-            </ul>
-        </aside>
-        <article class="full-article">
-            <nav class="breadcrumbs">
-                <ul>
-                    <li>Home</li>
-                    <li>Articles</li>
-                    <li>Category</li>
-                </ul>
-            </nav>
-            <section class="article-header">
-
-                <?php
-                if (have_posts()) {
-                    while (have_posts()) {
-                        the_title();
-                        get_template_part('template-parts/content', 'article');
-                        get_template_part('template-parts/content', 'gallery');
-                    }
-                }
-                ?>
-
-                <h1>Article Title</h1>
-                <div>
-                    <div class="tags-container">
-                        <div class="tag">
-                            <span>Tag</span>
-                        </div>
-                        <div class="tag">
-                            <span>Another tag</span>
-                        </div>
-                        <div class="tag">
-                            <span>Different tag</span>
-                        </div>
+    <section class="article-header">
+        <main>
+            <?php
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+                    $title = get_the_title();
+                    $content = get_the_content();
+            ?>
+                    <h1><?php echo $title; ?></h1>
+                    <div class="content">
+                        <?php echo $content; ?>
                     </div>
-                    <span class="article-date">Monday 27 May 2024</span>
-                </div>
-                <div class="image-container">
-                    <img src="./assets/images-icons/images/article-photo-1.webp" alt="" />
-                </div>
-            </section>
 
-        </article>
+            <?php endwhile;
+
+            else :
+                echo '<p>No content found.</p>';
+            endif;
+
+            // Reset the Post Data
+            wp_reset_postdata();
+            ?>
+
+
+
+
+            <div>
+                <div class="tags-container">
+                    <div class="tag">
+                        <span>Tag</span>
+                    </div>
+                    <div class="tag">
+                        <span>Another tag</span>
+                    </div>
+                    <div class="tag">
+                        <span>Different tag</span>
+                    </div>
+                </div>
+                <span class="article-date">Monday 27 May 2024</span>
+            </div>
+            <div class="image-container">
+                <img src="./assets/images-icons/images/article-photo-1.webp" alt="" />
+            </div>
+    </section>
+
+    </article>
     </div>
 </main>
 
 
 
-<?php
-get_footer();
-?>
+<?php get_footer(); ?>
