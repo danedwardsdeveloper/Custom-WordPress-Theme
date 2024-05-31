4465800e33843d2441197b628befe208e382cdb8
Dan Edwards <daniel.edwards96@yahoo.com>
2024-05-31 14:49:39 +0100

    Convert SCSS to LESS

--- themes/dan-digresses-theme/functions.php
+++ themes/dan-digresses-theme/functions.php
@@ -26,7 +26,7 @@ add_action("init", "dan_digresses_menus");
 function dan_digresses_register_styles()
 {
     $version = wp_get_theme()->get('Version');
-    wp_enqueue_style('danDigressesStyleSheet', get_stylesheet_directory_uri() . '/assets/css/style.min.css', array(), $version, "all");
+    wp_enqueue_style('danDigressesStyleSheet', get_stylesheet_directory_uri() . '/assets/css/style.css', array(), $version, "all");
 }
 add_action("wp_enqueue_scripts", "dan_digresses_register_styles");
 
